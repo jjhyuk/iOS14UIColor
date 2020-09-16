c193746e576bdcc9abf34b4527aa37e818b320dd
jinhyuk <jjhyuk15@gmail.com>
2020-09-16 14:25:35 +0900

    Initial Commit

---
+++ Xcode12Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode12Sample
+//
+//  Created by jinhyuk on 2020/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
